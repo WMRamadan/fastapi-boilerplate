fd556e5147f24e7e1ed0b15a438efae03fd1d284
Wael Ramadan <thebigguyis@gmail.com>
2022-12-10 00:52:15 +0200

    docs: add crud helper docstrings

--- api/helpers/crud.py
+++ api/helpers/crud.py
@@ -3,18 +3,39 @@ from .. import models, schemas
 
 
 def get_user(db: Session, user_id: int):
+    """
+    Get user by User ID.
+    :param db: The database session.
+    :param user_id: The User ID.
+    """
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
+    """
+    Get user by User email.
+    :param db: The database session.
+    :param email: The User Email.
+    """
     return db.query(models.User).filter(models.User.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Get all users.
+    :param db: The database session.
+    :param skip: The offset used when paging.
+    :param limit: The number of users to retrieve per query.
+    """
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
+    """
+    Create user.
+    :param db: The database session.
+    :param user: The user schema.
+    """
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
@@ -24,10 +45,22 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Get all items.
+    :param db: The database session.
+    :param skip: The offset used when paging.
+    :param limit: The number of items to retrieve per query.
+    """
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    """
+    Create the user item.
+    :param db: The database session.
+    :param item: The item schema.
+    :param limit: The User ID to add the item to.
+    """
     db_item = models.Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
