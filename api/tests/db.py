419f467cf849debba32d46c3103875c3436d35ed
Wael Ramadan <thebigguyis@gmail.com>
2022-12-11 00:52:39 +0200

    refactor: test db

---
+++ api/tests/db.py
@@ -0,0 +1,27 @@
+"""Test Database configuration."""
+#--------------------------------------------#
+# PEP-8 Imports Priority.
+# 1.Standard Library Imports
+# 2.Related Library Imports
+# 3.Local application/library imports
+#--------------------------------------------#
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def override_get_db():
+    """
+    Override database with test database.
+    """
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -6,32 +6,10 @@
 # 3.Local application/library imports
 #--------------------------------------------#
 import pytest
-import logging
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from api.database import Base, get_db
 from api import main
-
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-logger = logging.getLogger(__name__)
-
-def override_get_db():
-    """
-    Override database with test database.
-    """
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+from api.tests.db import engine, override_get_db
 
 @pytest.fixture()
 def test_db():
