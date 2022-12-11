2b85e3add02211a19f1cf6e6ed27d3e3688cb52b
Wael Ramadan <thebigguyis@gmail.com>
2022-12-11 03:42:36 +0200

    update: add VERSION

--- api/main.py
+++ api/main.py
@@ -5,6 +5,7 @@
 # 2.Related Library Imports
 # 3.Local application/library imports
 #--------------------------------------------#
+import os
 import time
 import asyncio
 from fastapi import FastAPI
@@ -22,6 +23,11 @@ app.include_router(tasks.router)
 
 logger.add("log_api.log", rotation="100 MB")    # Automatically rotate log file
 
+def get_version():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    with open(os.path.join(BASE_DIR, 'VERSION'), 'r') as fh:
+        version = fh.read().strip()
+    return version
 
 @app.get("/")
 async def root():
@@ -29,11 +35,12 @@ async def root():
     Root router.
     """
     logger.info("this is a info line")
+    version = get_version()
     start = time.time()
     a,b = await asyncio.gather(*[async_helper.async_func_a(2), async_helper.async_func_b(4)])
     end = time.time()
     result = {
-        "message": "Hello from the FastAPI Boilerplate!",
+        "message": f"Hello from the FastAPI Boilerplate v{version}!",
         "async_func_a": a,
         "async_func_b": b,
         "async_total_time": "All functions took {} seconds.".format(round(end-start))
